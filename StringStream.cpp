54fd73319dfc0f882348452c218ab549f320e6d6
Gayatribhosale2610 <65784117+Gayatribhosale2610@users.noreply.github.com>
2020-07-25 00:18:11 +0530

    Update StringStream.cpp

--- StringStream.cpp
+++ StringStream.cpp
@@ -1,44 +1,25 @@
  #include<sstream>
- 
  #include<vector>
-    
  #include<iostream>
- 
 using namespace std;
-
 vector <int> parseInts(const string& str) {
-
     vector <int> v;
-
     stringstream cin(str);
-    
     char ch;
-    
     int temp;
-    
     while(cin >> temp)
     {
-        
-        v.push_back(temp);
-        
+        v.push_back(temp);       
         cin >>ch;
     }
-    
     return v;
 }
-
-int main() {
-    
+int main() {   
     string str;
-    
     cin >> str;
-    
     vector<int> integers = parseInts(str);
-    
     for(int i = 0; i < integers.size(); i++) {
-     
         cout << integers[i] << "\n";
     }
-
     return 0;
 }
