964effb0eb4b6be47ab0196184733ddfb0cece5e
Gayatribhosale2610 <65784117+Gayatribhosale2610@users.noreply.github.com>
2020-07-25 00:21:41 +0530

    Update and rename String.md to String.py

--- String.py
+++ String.py
@@ -1,17 +1,9 @@
 def reverse(str):
-   
    s = ""
-   
    for ch in str:
-        
          s = ch + s
-   
    return s
 
-
 mystr = "programmer"
-
 print("Given String: ", mystr)
-
-
 print("Reversed String: ", reverse(mystr))
